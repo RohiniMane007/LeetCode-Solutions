d4e1a85c138fc9642e6d33fdd9b53f24553d4472
RohiniMane007 <manerohini295@gmail.com>
2024-12-08 17:01:05 +0530

    new

---
+++ Easy/String/Find the Difference/find_difference.dart
@@ -0,0 +1,18 @@
+// import "dart:math";
+void main() {
+  Solution s = Solution();
+  String str = s.findTheDifference("abcd", "abcde");
+
+  print(str);
+}
+
+class Solution {
+  String findTheDifference(String s, String t) {
+//      int maxLen = max(s.length,t.length);
+//      print(maxLen);
+
+    var str = s.split('');
+    print(str);
+    return "";
+  }
+}
