35306aa89b69ca2d41678045974e557ff43a16ff
RohiniMane007 <manerohini295@gmail.com>
2024-12-10 16:21:40 +0530

    change

--- Easy/String/Find the Difference/find_difference.dart
+++ Easy/String/Find the Difference/find_difference.dart
@@ -1,22 +1,21 @@
 // import "dart:math";
 void main() {
   Solution s = Solution();
-  String str = s.findTheDifference("a", "aa");
+  String str = s.findTheDifference("abcd", "abcde");
 
   print(str);
 }
 
 class Solution {
   String findTheDifference(String s, String t) {
-    for (int i = 0; i < s.length; i++) {
-      // if (t.contains(s[i])) {
-      //   t = t.replaceAll(s[i], '');
-      // }
-      if (s[i] == t[i]) {
-        // t = t.replaceFirstMapped(t[i],t, i);
+    List<String> sList = s.split('')..sort();
+    List<String> tList = t.split('')..sort();
+    for (int i = 0; i < sList.length; i++) {
+      if (sList[i] != tList[i]) {
+        return tList[i];
       }
     }
-    print(t);
-    return "";
+
+    return tList.last;
   }
 }
