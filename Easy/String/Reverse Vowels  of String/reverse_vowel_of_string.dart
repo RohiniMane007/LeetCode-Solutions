06d68e7546005c21a2820f31ef907b82daa693dd
RohiniMane007 <manerohini295@gmail.com>
2024-12-13 10:08:05 +0530

    change

--- Easy/String/Reverse Vowels  of String/reverse_vowel_of_string.dart
+++ Easy/String/Reverse Vowels  of String/reverse_vowel_of_string.dart
@@ -7,7 +7,7 @@ void main() {
 
 class Solution {
   String reverseVowels(String s) {
-    /* String vowel = "aeiouAEIOU";
+    String vowel = "aeiouAEIOU";
     List<String> olist = s.split('');
     List<String> revlist =
         s.split('').reversed.where((e) => vowel.contains(e)).toList();
@@ -19,23 +19,6 @@ class Solution {
         y++;
       }
     }
-    return olist.join('');*/
-
-    String vowel = "aeiouAEIOU";
-    List<String> olist = s.split('');
-    List<String> revlist = s.split('').reversed.toList();
-
-    for (int i = 0; i < s.length; i++) {
-      if (vowel.contains(olist[i])) {
-        for (int j = i; j < revlist.length; j++) {
-          if (vowel.contains(revlist[j])) {
-            olist[i] = revlist[j];
-            break;
-          }
-        }
-      }
-    }
-
     return olist.join('');
   }
 }
