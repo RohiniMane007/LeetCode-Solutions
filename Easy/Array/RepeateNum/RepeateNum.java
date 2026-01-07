b9aebb8d15b00334224c0b6fb4ce16df9ce95366
Rohini Mane <123631971+RohiniMane007@users.noreply.github.com>
2026-01-07 17:19:16 +0530

    removing comments
    Removed commented-out code and cleaned up print statements.

--- Easy/Array/RepeateNum/RepeateNum.java
+++ Easy/Array/RepeateNum/RepeateNum.java
@@ -4,17 +4,10 @@ import java.util.Set;
 public class RepeateNum {
     public static void main(String[] args) {
       int result =  repeatedNTimes(new int[]{1,2,3,3});
-      System.out.println("Result: " + result);
-        
+      System.out.println("Result: " + result);   
     }
 
     public static int repeatedNTimes1(int[] nums) {
-
-        // int len = nums.length;
-        // int n = len / 2;
-        // System.out.println("n: " + n );
-        // System.out.println("n+1: " + (n+1));
-
         for(int i =0; i< nums.length - 1; i++){
             for(int j = i + 1; j< nums.length; j++){
                 if(nums[i]== nums[j]){
@@ -26,20 +19,6 @@ public class RepeateNum {
     }
 
      public static int repeatedNTimes(int[] nums) {
-
-       
-        // System.out.println("n: " + n );
-        // System.out.println("n+1: " + (n+1));
-        // HashSet<Integer> set = new HashSet<>();
-        
-        // for(int i = 0; i < nums.length ; i++){
-        //     set.add(nums[i]);
-        //          System.out.println("n: " + nums[i] );
-        //         if(!set.contains(nums[i])==true){
-        //             return nums[i];
-        //         }       
-        // }
-
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             System.out.println("n: " + num );
